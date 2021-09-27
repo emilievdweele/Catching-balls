aab954a7e10966748a6d5ac6cb3ff8a60c0c4622
emilievdweele <74767348+emilievdweele@users.noreply.github.com>
2021-09-27 13:46:05 +0000

    deel van balken

--- script.js
+++ script.js
@@ -20,21 +20,40 @@ class Ball {
   }
 }
 
+class Balken {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  drawBalken() {
+    rect(this.x, this.y, this.w, this.h)
+    this.x -= 5
+  }
+}
+
 var ball1;
 
 function setup() {
   createCanvas(800, 400);
 
-  ball1 = new Ball(150, 200, 30, 30, 3)
+  ball1 = new Ball(150, 200, 30, 30, 2)
 }
 
 function draw() {
   background(225);
   ball1.drawBall();
+
+if(frameCount % 100 == 0){
+  rect = new Balken(random)
+}
+
 }
 
 function keyPressed() {
   if(keyCode == 32){
-    ball1.y -= 50;
+    ball1.y -= 60;
   }
 }
