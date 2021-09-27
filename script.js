63aa08917aa043ff7ce7c47b2891bcd27d9638fe
emilievdweele <74767348+emilievdweele@users.noreply.github.com>
2021-09-27 13:24:42 +0000

    balletje valt

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 class Ball {
-  constructor(x, y, w, h) {
+  constructor(x, y, w, h, gravity) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
+  this.gravity = gravity;
   }
 
   drawBall() {
-  ellipse(this.x, this.y, this.w, this.h)
+    ellipse(this.x, this.y, this.w, this.h)
+    this.y = this.y + this.gravity;
+  
+    if(this.y > 380) {
+      this.gravity = 0;
+    }
+    if(this.y < 0) {
+      this.y = 20;
+    }
   }
 }
+
+var ball1;
 
 function setup() {
   createCanvas(800, 400);
 
-  ball1 = new Ball(150, 200, 30, 30)
+  ball1 = new Ball(150, 200, 30, 30, 3)
 }
 
 function draw() {
